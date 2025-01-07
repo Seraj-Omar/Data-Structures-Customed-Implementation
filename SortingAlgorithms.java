932e53a3729e67aa7033e054b157e7917b1a3566
Seraj Omar <153351518+Seraj-Omar@users.noreply.github.com>
2025-01-07 18:44:41 +0200

    Update SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,158 +1,160 @@
-public class SortingAlgorithms {
+public class SortingAlgorithms<T extends Comparable<T>> {
 
     // Bubble Sort
-    public void bubbleSort(int[] arr) {
-        int n = arr.length;
+    public void bubbleSort(T[] a) {
+        int n = a.length;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if (a[j].compareTo(a[j + 1]) > 0) {
+                    T temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
                 }
             }
         }
     }
 
     // Selection Sort
-    public void selectionSort(int[] arr) {
-        int n = arr.length;
+    public void selectionSort(T[] a) {
+        int n = a.length;
         for (int i = 0; i < n - 1; i++) {
             int minIdx = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIdx]) {
+                if (a[j].compareTo(a[minIdx]) < 0) {
                     minIdx = j;
                 }
             }
-            int temp = arr[minIdx];
-            arr[minIdx] = arr[i];
-            arr[i] = temp;
+            T temp = a[minIdx];
+            a[minIdx] = a[i];
+            a[i] = temp;
         }
     }
 
     // Insertion Sort
-    public void insertionSort(int[] arr) {
-        int n = arr.length;
+    public void insertionSort(T[] a) {
+        int n = a.length;
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
+            T key = a[i];
             int j = i - 1;
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
+            while (j >= 0 && a[j].compareTo(key) > 0) {
+                a[j + 1] = a[j];
                 j = j - 1;
             }
-            arr[j + 1] = key;
+            a[j + 1] = key;
         }
     }
 
     // Merge Sort
-    public void mergeSort(int[] arr) {
-        if (arr.length < 2) {
+    public void mergeSort(T[] a) {
+        if (a.length < 2) {
             return;
         }
-        int mid = arr.length / 2;
-        int[] left = new int[mid];
-        int[] right = new int[arr.length - mid];
+        int mid = a.length / 2;
+        T[] left = (T[]) new Comparable[mid];
+        T[] right = (T[]) new Comparable[a.length - mid];
 
-        System.arraycopy(arr, 0, left, 0, mid);
-        System.arraycopy(arr, mid, right, 0, arr.length - mid);
+        System.arraycopy(a, 0, left, 0, mid);
+        System.arraycopy(a, mid, right, 0, a.length - mid);
 
         mergeSort(left);
         mergeSort(right);
-        merge(arr, left, right);
+        merge(a, left, right);
     }
 
-    private void merge(int[] arr, int[] left, int[] right) {
+    private void merge(T[] a, T[] left, T[] right) {
         int i = 0, j = 0, k = 0;
         while (i < left.length && j < right.length) {
-            if (left[i] <= right[j]) {
-                arr[k++] = left[i++];
+            if (left[i].compareTo(right[j]) <= 0) {
+                a[k++] = left[i++];
             } else {
-                arr[k++] = right[j++];
+                a[k++] = right[j++];
             }
         }
         while (i < left.length) {
-            arr[k++] = left[i++];
+            a[k++] = left[i++];
         }
         while (j < right.length) {
-            arr[k++] = right[j++];
+            a[k++] = right[j++];
         }
     }
 
     // Quick Sort
-    public void quickSort(int[] arr, int l, int r) {
+    public void quickSort(T[] a, int l, int r) {
         if (l < r) {
-            int pi = partition(arr, l, r);
-            quickSort(arr, l, pi - 1);
-            quickSort(arr, pi + 1, r);
+            int pi = partition(a, l, r);
+            quickSort(a, l, pi - 1);
+            quickSort(a, pi + 1, r);
         }
     }
 
-    private int partition(int[] arr, int l, int r) {
-        int pivot = arr[r];
+    private int partition(T[] a, int l, int r) {
+        T pivot = a[r];
         int i = (l - 1);
         for (int j = l; j < r; j++) {
-            if (arr[j] < pivot) {
+            if (a[j].compareTo(pivot) < 0) {
                 i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                T temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[r];
-        arr[r] = temp;
+        T temp = a[i + 1];
+        a[i + 1] = a[r];
+        a[r] = temp;
         return i + 1;
     }
 
     // Heap Sort
-    public void heapSort(int[] arr) {
-        int n = arr.length;
+    public void heapSort(T[] a) {
+        int n = a.length;
         for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(arr, n, i);
+            heapify(a, n, i);
         }
         for (int i = n - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-            heapify(arr, i, 0);
+            T temp = a[0];
+            a[0] = a[i];
+            a[i] = temp;
+            heapify(a, i, 0);
         }
     }
 
-    private void heapify(int[] arr, int n, int i) {
+    private void heapify(T[] a, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
 
-        if (left < n && arr[left] > arr[largest]) {
+        if (left < n && a[left].compareTo(a[largest]) > 0) {
             largest = left;
         }
-        if (right < n && arr[right] > arr[largest]) {
+        if (right < n && a[right].compareTo(a[largest]) > 0) {
             largest = right;
         }
         if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-            heapify(arr, n, largest);
+            T swap = a[i];
+            a[i] = a[largest];
+            a[largest] = swap;
+            heapify(a, n, largest);
         }
     }
 
+    // Main method for testing
     public static void main(String[] args) {
-        SortingAlgorithms sorter = new SortingAlgorithms();
-        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        SortingAlgorithms<Integer> sorter = new SortingAlgorithms<>();
+        Integer[] a = {64, 34, 25, 12, 22, 11, 90};
 
         System.out.println("Original array:");
-        for (int n : arr) {
+        for (int n : a) {
             System.out.print(n + " ");
         }
         System.out.println();
 
-        sorter.bubbleSort(arr);
+        sorter.bubbleSort(a);
         System.out.println("Sorted array using Bubble Sort:");
-        for (int num : arr) {
-            System.out.print(num + " ");
+        for (int n : a) {
+            System.out.print(n + " ");
         }
         System.out.println();
+
         // Add calls to other sorting methods here for testing
     }
 }
